97a6d4e24d29276d335289f0bfdd3a2b824623d1
Travis Scott <travisscott301@gmail.com>
2014-11-30 23:48:25 -0600

    Created a basic debug game world

--- core/src/com/packtpub/libgdx/canyonbunny/CanyonBunnyMain.java
+++ core/src/com/packtpub/libgdx/canyonbunny/CanyonBunnyMain.java
@@ -1,5 +1,6 @@
 package com.packtpub.libgdx.canyonbunny;
 
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
@@ -12,33 +13,60 @@ public class CanyonBunnyMain extends ApplicationAdapter {
 	
 	private WorldController worldController;
 	private WorldRenderer worldRenderer;
+	private boolean paused;
 	
 	@Override
 	public void create () {
+		//Set Libgdx Log level to DEBUG
+		Gdx.app.setLogLevel(Application.LOG_DEBUG);
 		
+		//Initialize controller and renderer
+		worldController = new WorldController();
+		worldRenderer = new WorldRenderer(worldController);
+		
+		//Game world is active on start
+		paused = false;
 	}
 
 	@Override
 	public void render () {	
+		//Do not update game world when paused
+		if (!paused) {
+			//Update game world by the time that has passed since the last frame
+			worldController.update(Gdx.graphics.getDeltaTime());
+		}
+		
+		//Sets the clear screen color to: Cornflower blue
+		Gdx.gl.glClearColor(0x64/255.0f, 0x95/255.0f, 0xed/255.0f, 0xff/255.0f);
+		
+		//clears the screen
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		//Render the game world to screen
+		worldRenderer.render();
 	}
 
 	@Override
 	public void resize(int width, int height) {
 		super.resize(width, height);
+		worldRenderer.resize(width, height);
 	}
 
 	@Override
 	public void pause() {
 		super.pause();
+		paused = true;
 	}
 
 	@Override
 	public void resume() {
 		super.resume();
+		paused = false;
 	}
 
 	@Override
 	public void dispose() {
 		super.dispose();
+		worldRenderer.dispose();
 	}
 }
--- core/src/com/packtpub/libgdx/canyonbunny/WorldRenderer.java
+++ core/src/com/packtpub/libgdx/canyonbunny/WorldRenderer.java
@@ -1,8 +1,10 @@
 package com.packtpub.libgdx.canyonbunny;
 
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.Disposable;
+import com.packtpub.libgdx.canyonbunny.util.Constants;
 
 public class WorldRenderer implements Disposable {
 	private OrthographicCamera camera;
@@ -13,11 +15,37 @@ public class WorldRenderer implements Disposable {
 	public WorldRenderer(WorldController worldController) {
 		super();
 		this.worldController = worldController;
+		init();
+	}
+	
+	private void init() {
+		batch = new SpriteBatch();
+		camera = new OrthographicCamera(Constants.VIEWPORT_WIDTH, Constants.VIEWPORT_HEIGHT);
+		camera.position.set(0, 0, 0);
+		camera.update();
+	}
+	
+	public void render() {
+		renderTestObjects();
+	}
+	
+	private void renderTestObjects() {
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+		for(Sprite sprite : worldController.testSprites) {
+			sprite.draw(batch);
+		}
+		batch.end();
+	}
+	
+	public void resize(int width, int height) {
+		camera.viewportWidth = (Constants.VIEWPORT_HEIGHT / height) * width;
+		camera.update();
 	}
 	
 	@Override
 	public void dispose() {
-		
+		batch.dispose();
 	}
 	
 	
